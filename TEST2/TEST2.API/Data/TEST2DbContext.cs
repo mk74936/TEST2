f9ee881332311cb5bcb7b541d2de0867b0b4ebf2
mk74936 <kumarmanojdoppalapudi@gmail.com>
2022-11-01 14:34:22 +0530

    End of Session 2

---
+++ TEST2/TEST2.API/Data/TEST2DbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TEST2.API.Models.Domain;
+
+namespace TEST2.API.Data
+{
+    public class TEST2DbContext:DbContext
+    {
+        public TEST2DbContext(DbContextOptions<TEST2DbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ TEST2/TEST2.API/Migrations/20221101090204_InitalSetup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TEST2.API.Migrations
+{
+    public partial class InitalSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
